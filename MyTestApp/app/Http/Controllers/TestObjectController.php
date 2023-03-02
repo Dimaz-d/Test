784177bf18dec7374dc7d95193046ee768b7d53b
Dimaz-d <vinekaster@gmail.com>
2023-03-02 18:50:34 +0300

    Fix code style

--- MyTestApp/app/Http/Controllers/TestObjectController.php
+++ MyTestApp/app/Http/Controllers/TestObjectController.php
@@ -16,34 +16,27 @@ class TestObjectController extends Controller
         if (!$user) {
             return response()->json(['message' => 'Unauthorized'], 401);
         }
-        // Check if request is GET or POST
-            // Save object to database
-            $data = $request->input('data');
-            $object = TestObject::find($request->input('id'));
-
-            if($object){
-                if($object->user_id == $user->id){
-                    $object->fill(json_decode($data, true));
-                    $object->user_id = $user->id;
-                    $object->save();
-                    // Return object ID and processing time and memory
-                    $id = $object->id;
-                    $processing_time = microtime(true) - LARAVEL_START;
-                    $memory_usage = memory_get_usage(true) / 1024 / 1024;
-                    $response = [
-                        'id' => $id,
-                        'processing_time' => $processing_time,
-                        'memory_usage' => $memory_usage,
-                    ];
-                    return response()->json($response);
-                } else{
-                    return response()->json(['message' => 'Invalid id'], 410);
-                }
-            }else{
-                return response()->json(['message' => 'Invalid id'], 410);
-            }
-
+        $data = $request->input('data');
+        $object = TestObject::where('user_id', $user->id)->where('id', $request->input('id'))->first();
+        if ( is_null($object)) {
+            return response()->json(['message' => 'Invalid id'], 410);
+        }
+        // Edit object in database
+        $object->fill(json_decode($data, true));
+        $object->user_id = $user->id;
+        $object->save();
+        // Return object ID and processing time and memory
+        $id = $object->id;
+        $processing_time = microtime(true) - LARAVEL_START;
+        $memory_usage = memory_get_usage(true) / 1024 / 1024;
+        $response = [
+            'id' => $id,
+            'processing_time' => $processing_time,
+            'memory_usage' => $memory_usage,
+        ];
+        return response()->json($response);
     }
+
     public function createData(Request $request)
     {
         // Authenticate user based on token in header
@@ -51,21 +44,21 @@ class TestObjectController extends Controller
         if (!$user) {
             return response()->json(['message' => 'Unauthorized'], 401);
         }
-            // Save object to database
-            $data = $request->input('data');
-            $object = new TestObject();
-            $object->fill(json_decode($data, true));
-            $object->user_id = $user->id;
-            $object->save();
-            // Return object ID and processing time and memory
-            $id = $object->id;
-            $processing_time = microtime(true) - LARAVEL_START;
-            $memory_usage = memory_get_usage(true) / 1024 / 1024;
-            $response = [
-                'id' => $id,
-                'processing_time' => $processing_time,
-                'memory_usage' => $memory_usage,
-            ];
-            return response()->json($response);
+        // Save object to database
+        $data = $request->input('data');
+        $object = new TestObject();
+        $object->fill(json_decode($data, true));
+        $object->user_id = $user->id;
+        $object->save();
+        // Return object ID and processing time and memory
+        $id = $object->id;
+        $processing_time = microtime(true) - LARAVEL_START;
+        $memory_usage = memory_get_usage(true) / 1024 / 1024;
+        $response = [
+            'id' => $id,
+            'processing_time' => $processing_time,
+            'memory_usage' => $memory_usage,
+        ];
+        return response()->json($response);
     }
 }
